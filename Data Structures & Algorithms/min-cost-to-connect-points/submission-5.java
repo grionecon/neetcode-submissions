3d769b2e4932e1a1f9f49a8805be1711a10ddbe6
grionecon <106099598+grionecon@users.noreply.github.com>
2026-06-27 01:50:36 +0200

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.java
@@ -0,0 +1,41 @@
+class Solution {
+    class Edge {
+        int p1;
+        int p2;
+        int diff;
+        Edge(int p1, int p2, int diff) {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.diff = diff;
+        }
+    }
+    public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> {
+            return a.diff - b.diff;
+        });
+        int[] coordinate = points[0];
+        for (int i = 1; i < points.length; i++) {
+                pq.add(new Edge(0, i, Math.abs(coordinate[0] - points[i][0]) + Math.abs(coordinate[1] - points[i][1])));
+        }
+
+        int num = points.length - 1;
+        int sum = 0;
+        boolean[] visited = new boolean[points.length];
+        visited[0] = true;
+        while (!pq.isEmpty() && num > 0) {
+            Edge e = pq.remove();
+            if (visited[e.p2]) {
+                continue;
+            }
+            visited[e.p2] = true;
+            num -= 1;
+            sum += e.diff;
+            for (int i = 0; i < points.length; i++) {
+                if (!visited[i]) {
+                    pq.add(new Edge(e.p2, i, Math.abs(points[e.p2][0] - points[i][0]) + Math.abs(points[e.p2][1] - points[i][1])));
+                }
+            }
+        }
+        return sum;
+    }
+}
