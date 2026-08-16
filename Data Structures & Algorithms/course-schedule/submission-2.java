ef94913b37b34bcdb8a0ca28173e68fb09d499fa
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-17 01:33:37 +0200

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    Set<Integer> visited;
+    ArrayList<Integer>[] adjList;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+        visited = new HashSet<Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            var result = dfs(i);
+            if (!result) {
+                return false;
+            }
+        }
+        return true;
+    }
+    boolean dfs(int node) {
+        if (visited.contains(node)) {
+            return false;
+        }
+        if (adjList[node].size() == 0) {
+            return true;
+        }
+        visited.add(node);
+        var success = true;
+        for (int i = 0; i < adjList[node].size(); i++) {
+            if (!dfs(adjList[node].get(i))) {
+                return false;
+            }
+        }
+        visited.remove(node);
+        adjList[node] = new ArrayList<>();
+        return success;
+    }
+}
