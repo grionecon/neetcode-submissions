ded3a79c9a5d00949b19597fd92960a55fe36ede
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-19 01:51:21 +0200

    Add: course-schedule - submission-14

---
+++ Data Structures & Algorithms/course-schedule/submission-14.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer> adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<Integer>();
+        }
+        //The indegree of a vertex in a directed graph is the total number of incoming edges pointing directly into that specific vertex
+        var indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList[prerequsites[i][0]].add(prerequsites[i][1]);
+            indegree[prerequsites[i][1]] += 1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        var visitedEdgeCount = 0;
+        while (!q.isEmpty()) {
+            var currEdge = q.remove();
+            visitedEdgeCount += 1;
+            for (var connection : adjList[currEdge]) {
+                indegree[connection] -= 1;
+                if (indegree[connection] == 0) {
+                    q.add(connection);
+                }
+            }
+        }
+        return visitedEdgeCount == numCourses;
+    }
+}
