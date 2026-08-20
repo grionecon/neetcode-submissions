e6d27e4d3f4eb1621296656d1ed604b3a486bd58
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-21 01:45:52 +0200

    Add: course-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-7.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    List<Integer>[] adjList;
+    int[] indegree;
+    List<Integer> output;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList[prerequisites[i][0]].add(prerequisites[i][1]);
+            indegree[prerequisites[i][1]] += 1;
+        }
+        output = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                dfs(i);
+            }
+        }
+        if (output.size() != numCourses) {
+            return new int[0];
+        }
+        var result = new int[numCourses];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = output.get(numCourses - i - 1);
+        }
+        return result;
+    }
+    public void dfs(int i) {
+        output.add(i);
+        indegree[i] -= 1;
+        for (var conn : adjList[i]) {
+            indegree[conn] -= 1;
+            if (indegree[conn] == 0) {
+                dfs(conn);
+            }
+        }
+    }
+}
