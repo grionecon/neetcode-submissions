50ca9ab421334e3d37dbf149ec1ba870e6683140
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-22 15:32:17 +0200

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.java
@@ -0,0 +1,53 @@
+class Solution {
+    List<Integer>[] adjList;
+    int[] indegree;
+    boolean[] visiting;
+    boolean[] visited;
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList();
+        }
+        indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList[prerequisites[i][0]].add(prerequisites[i][1]);
+            indegree[prerequisites[i][1]] += 1;
+        }
+        List<Boolean> res = new ArrayList<>();
+        for (int i = 0; i < queries.length; i++) {
+            var start = queries[i][0];
+            var finish = queries[i][1];
+            visiting = new boolean[numCourses];
+            visited = new boolean[numCourses];
+            if (dfs(start, finish) == finish) {
+                adjList[start].add(finish);
+                res.add(true);
+            } else {
+                res.add(false);
+            }
+        }
+        return res;
+    }
+    int dfs(int i, int target) {
+        if (visited[i]) {
+            return -2;
+        }
+        if (i == target) {
+            return target;
+        }
+        visiting[i] = true;
+        for (var conn: adjList[i]) {
+            if (!visiting[conn]) {
+                var res = dfs(conn, target);
+                if (res == -2) {
+                    return -2;
+                } else if (res > 0) {
+                    return res;
+                }
+            }
+        }
+        visited[i] = true;
+        visiting[i] = false;
+        return -1;
+    }
+}
