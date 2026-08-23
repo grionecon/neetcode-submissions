2bd9e87cc1cf1b1d765c931f1ef5d7049abd7ba8
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-24 01:17:30 +0200

    Add: minimum-height-trees - submission-7

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) {
+            var res = Arrays.asList(new int[]{0});
+        }
+        List<Integer>[] adjList = new List[n];
+        for (int i = 0; i < n; i++) {
+
+        }
+        return new ArrayList<>();
+    }
+}
