60a0ba1da1c1bb15004162413125eec21ce50cc8
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-27 14:27:32 +0200

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        var arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 1;
+        for (int i = 3; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+        }
+        return arr[n];
+    }
+}
