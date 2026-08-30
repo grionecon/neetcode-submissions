ecee1b171a0fd66b20d7c3983a86d3b98b315a71
grionecon <106099598+grionecon@users.noreply.github.com>
2026-08-31 01:14:31 +0200

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        var res = new int[nums.length];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        res[0] = nums[0];
+        res[1] = Integer.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            res[i] = Integer.max(nums[i] + nums[i - 2], nums[i - 1]);
+        }
+        return res[nums.length - 1];
+    }
+}
